8a8abd5d738510c53a0f8ab5214bf4553870f912
yechan <100554186+ykim370@users.noreply.github.com>
2023-10-16 13:26:28 +1300

    Update main.c

--- Code testing/main.c
+++ Code testing/main.c
@@ -1,55 +1,36 @@
-/*
- * Tawans_code.c
- *
- * Created: 4/10/2023 3:29:04 pm
- * Author : vsan396
- */ 
+* Author : Yechan
 
-//INCLUDES
+#include "UART.h"
 #include <avr/io.h>
 #include <stdint.h>
-#include <avr/interrupt.h>
-#include "adc.h"
-#include "timer0.h"
-#include "PI_control.h"
+#include <string.h>
 
-//DEFINES
-#define F_CPU 16000000UL
+#define F_CPU 16000000
+#define BAUD_RATE 9600
+#define TX_BUFFER_SIZE 9
+#define RX_BUFFER_SIZE 9
 
-//GLOBALS
-
-volatile static uint16_t ADC_VALUE;	
-static volatile uint16_t V_ref = 3000; // 0-1024 is equiv
-volatile uint8_t ADC_flag = 0;
-void PIN_TOGGLE_D(uint8_t PIN){
-	PORTD ^= (1 << PIN);
-}	
-
-ISR(ADC_vect){
-	PIN_TOGGLE_D(PIND0); // toggle PB5 to indicate ISR trigger
-	ADC_VALUE = ADC; // Reading ADC register clears the ADC interrupt flag 
-	ADC_flag = 1; 
-	TIFR0 |= (1 << TOV0); //clears tc0 overflow flag
+void UART_init(void){
+	UCSR0B |= 1 << TXEN0 | 1 << RXEN0 | 1 << TXCIE0 | 1 << RXCIE0;
+	UCSR0C |= 3 << UCSZ00;
+	UBRR0 = F_CPU / ((uint32_t)16 * BAUD_RATE) - 1;
 }
 
-int main(void)
-{
+void uart_transmit_byte(char byte){
+	while (!(UCSR0A & (1 << UDRE0)));
+	UDR0 = byte;
+}
 
-	tc0_init_FPWM_100k(); //initialize PWM to generate 100KHz, 50% duty
-	adc_init(); //initializing ADC
-	DDRD |= (1 << PIND0); //set PIND0 as output
-	DDRD |= (1 << PIND2);
-	ENABLE_ADC_INTERRUPT;
-	sei();
-	
-    while (1) {
-		if(ADC_flag == 1){
-			PIN_TOGGLE_D(PIND2);
-			pi_controller(adc_to_mv(ADC_VALUE), V_ref);
-			pi_to_duty();
-			PIN_TOGGLE_D(PIND2);
-			ADC_flag = 0;	
-		}
+void uart_transmit_array(char* msg){
+	for (uint8_t i = 0; i < strlen(msg); i++){
+		uart_transmit_byte(msg[i]);
 	}
 }
 
+int uart_extract_digits(int value){
+	return uart_convert_digits(value % 10);
+}
+
+int uart_convert_digits(int digit){
+	return digit + 48;
+}
